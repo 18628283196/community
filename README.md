2d2fd9f9d806a617653f8a8d0446cb86e807eede
18628283196 <zl546505720@163.com>
2019-11-24 22:17:18 +0800

    实现用户登录并保存user信息

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 
 ## 资料
 [spring 文档](https://spring.io/guides) 
+
 [spring app](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)
 
-git commit --amend --no-edit
+[spring 官方文档](https://spring.io/projects/spring-boot)
+
+追加到上次的提交上去：git commit --amend --no-edit
 ## 工具
--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDTO;
 import life.majiang.community.dto.GithubUser;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,10 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
     @Value(value = "${github.client.id}")
     private String clientId;
     @Value(value = "${github.client.secret}")
@@ -23,7 +30,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -31,8 +39,22 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setClient_secret(secret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser getUser = githubProvider.getUser(accessToken);
+        System.out.println(getUser.getName());
+        if (getUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(getUser.getName());
+            user.setAccountId(String.valueOf(getUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(System.currentTimeMillis());
+            userMapper.insert(user);
+            //登录成功，写入cookie和Session
+            request.getSession().setAttribute("user", getUser);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登陆
+            return "redirect:/";
+        }
     }
 }
